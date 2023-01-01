31b873c18402df25ee97f0a31f18794b4e54e7dd
Akeem <akeem.mighty@gmail.com>
2023-01-01 14:46:56 -0500

    Created Graph Class + Added Mapping of Vertex With Adjacency List

--- BasicGraph.java
+++ BasicGraph.java
@@ -7,5 +7,9 @@ public class BasicGraph {
             this.label = label;
         }
     }
+
+    class Graph {
+        private Map<Vertex, List<Vertex>> adjVertices;
+    }
     public static void main(String[] args) {}
 }
