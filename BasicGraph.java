c725ab133d26690c1cfd5d05f7d2911f309f8264
Akeem <akeem.mighty@gmail.com>
2023-01-01 15:02:34 -0500

    Added addEdge Between Vertex Functionality

--- BasicGraph.java
+++ BasicGraph.java
@@ -15,15 +15,25 @@ public class BasicGraph {
         //Map takes parameters Vertex + adjacency list of vertex
         private Map<Vertex, List<Vertex>> adjVertices;
 
+        //Add Vertex while vertex does not exist
         void addVertex(String label) {
           adjVertices.putIfAbsent(new Vertex(label), new ArrayList<>());
         }
 
+        //Remove specified vertex
         void removeVertex(String label) {
             Vertex v = new Vertex(label);
 
             adjVertices.values().stream().forEach(e -> e.remove(v));
             adjVertices.remove(new Vertex(label));
+        }
+
+        //Add Edge between two Vertex
+        void addEdge(String label1, String label2){
+            Vertex v1 = new Vertex(label1);
+            Vertex v2 = new Vertex(label2);
+            adjVertices.get(v1).add(v2);
+            adjVertices.get(v2).add(v1);
         }
     }
     public static void main(String[] args) {}
