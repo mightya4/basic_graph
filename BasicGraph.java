1a13ddeb7184a7a43a29fb6b726e34462a68a30d
Akeem <akeem.mighty@gmail.com>
2023-01-01 15:07:28 -0500

    Added removeEdge Between Vertex Functionality

--- BasicGraph.java
+++ BasicGraph.java
@@ -35,6 +35,16 @@ public class BasicGraph {
             adjVertices.get(v1).add(v2);
             adjVertices.get(v2).add(v1);
         }
+
+        //Remove Edge between two Vertex
+        void removeEdge(String label1, String label2) {
+            Vertex v1 = new Vertex(label1);
+            Vertex v2 = new Vertex(label2);
+            List<Vertex> eV1 = adjVertices.get(v1);
+            List<Vertex> eV2 = adjVertices.get(v2);
+            if (eV1 != null) {eV1.remove(v2);}
+            if (eV2 != null) {eV2.remove(v1);}
+        }
     }
     public static void main(String[] args) {}
 }
