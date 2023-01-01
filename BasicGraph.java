55be8fe4fa0714b850ec2d1e43bab2df53957ff6
Akeem <akeem.mighty@gmail.com>
2023-01-01 14:44:32 -0500

    Create Vertex Class

--- BasicGraph.java
+++ BasicGraph.java
@@ -1,3 +1,11 @@
 public class BasicGraph {
+
+    //Build Vertex for each node of Graph
+    class Vertex {
+        String label;
+        Vertex(String label) {
+            this.label = label;
+        }
+    }
     public static void main(String[] args) {}
 }
