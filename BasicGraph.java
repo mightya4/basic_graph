09e95cb7fa748b01f0ed90171a215e6a2856c020
Akeem <akeem.mighty@gmail.com>
2023-01-01 22:39:01 -0500

    Removed Create Sample Graph Functionality

--- BasicGraph.java
+++ BasicGraph.java
@@ -51,22 +51,7 @@ public class BasicGraph {
             return adjVertices.get(new Vertex(label));
         }
 
-        //Create Sample Graph
-        Graph createGraph() {
-            Graph graph = new Graph();
-            graph.addVertex("Bob");
-            graph.addVertex("Alice");
-            graph.addVertex("Mark");
-            graph.addVertex("Rob");
-            graph.addVertex("Maria");
-            graph.addEdge("Bob", "Alice");
-            graph.addEdge("Bob", "Rob");
-            graph.addEdge("Alice", "Mark");
-            graph.addEdge("Rob", "Mark");
-            graph.addEdge("Alice", "Maria");
-            graph.addEdge("Rob", "Maria");
-            return graph;
-        }
+        
     }
     public static void main(String[] args) {
         
